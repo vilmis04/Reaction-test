a3812cdfd85cc1aecba284b2c8a1163369b45f9f
Vilmantas <vilmantas.sudaris@gmail.com>
2021-11-29 23:22:56 +0200

    [UPD] start btn, repo address and time calculation

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const MIN_SIZE = 100;
 const MAX_SIZE = 400;
 const MAX_WAIT = 2000; //in miliseconds, i.e., 3s = 3000ms;
-const trig = document.querySelector(".btn");
 
+const trig = document.querySelector(".btn");
+const duration = document.querySelector("span");
+const startBtn = document.querySelector(".start");
+
+let start;
 
 function setSize() {
     const size = MIN_SIZE + Math.floor(Math.random()*MAX_SIZE);
@@ -18,25 +22,34 @@ function setColor() {
 function setPosition() {
     trig.style.top = Math.round((window.innerHeight-MAX_SIZE)*Math.random())+"px";
     trig.style.left = Math.round((window.innerWidth-MAX_SIZE)*Math.random())+"px";
-
 }
 
 function setShape() {
     trig.style.borderRadius = Math.round(Math.random())*50+"%";
 }
 
+function startTimer () {
+    start = new Date();
+}
+
+function displayTimer() {
+    let end = new Date();
+    duration.textContent = (end.getTime() - start.getTime())/1000 + " s";
+}
+
 function cooldownTime() {
     trig.style.width = "0";
-    // displayTimer();
+    displayTimer();
     setTimeout(triggerChange, Math.round(Math.random()*MAX_WAIT));
 }
 
 function triggerChange() {
-    // startTimer();
     setSize();
     setColor();
     setPosition();
     setShape();
+    startTimer();
 }
 
+startBtn.addEventListener("click", triggerChange);
 trig.addEventListener("click", cooldownTime);
